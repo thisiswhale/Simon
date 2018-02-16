1936066669beb532077bf5b99426fc43dd20c204
William Leung <leungwk25@gmail.com>
2018-02-16 14:38:46 -0800

    Refractored code and completed computer sequencing

--- script.js
+++ script.js
@@ -1,34 +1,74 @@
-const simon1 = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
-const simon2 = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
-const simon3 = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
-const simon4 = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
 
-// const yellow = new Audio('http://res.cloudinary.com/thisiswhale/video/upload/v1518673264/Yellow_diamond._bhyqol.mp3');
-// const white = new Audio('http://res.cloudinary.com/thisiswhale/video/upload/v1518673204/White_diamond_chedsv.mp3');
-// const pink = new Audio('http://res.cloudinary.com/thisiswhale/video/upload/v1518673213/Pink_diamond_axisqu.mp3');
-// const blue = new Audio('http://res.cloudinary.com/thisiswhale/video/upload/v1518673212/Blue_diamond_wpstav.mp3');
-//simon1.play();
-
-const panels = document.getElementsByClassName('panel')
+let simonMemory = [];
+let playerMemory = [];
+let currentRound = 1;
 const totalRound = 20;
+let roundSpeedDuration = 1000;
+let playSpeed = 1;
+
+const animationDuration = 500;
+const gameboard = ['green','blue','red','yellow'];
+const panels = document.getElementsByClassName('panel');
+
 for (let i = 0; i < panels.length; i++){
-panels[i].addEventListener('click', playSound);
-
+panels[i].addEventListener('click', getPanel);
+// panels[i].addEventListener('click', checkMemory);
 }
-function playSound(){
+
+function getPanel(){
   const color = this.getAttribute('id');
-  const audio = document.querySelector(`audio[data-key="${color}"]`);
-  console.log(this)
-  audio.currentTime = 0;
-  audio.playbackRate = 0.75;
-  audio.play();
+  lightUp (color)
+  playSound(color)
+}
+
+function startGame(){
+    simonMemory = [];
+    playerMemory = [];
+    currentRound = 1;
+    newRound();
+}
+
+newRound();
+
+function newRound(){
+  const randomNum =Math.floor(Math.random() * 3);
+  simonMemory.push(gameboard[randomNum]);
+  console.log(randomNum, simonMemory)
+  animate(simonMemory)
+}
+
+function animate(sequence) { //sequence is an array
+    var i = 0;
+    var interval = setInterval(function() {
+        lightUp(sequence[i]);
+        playSound(sequence[i]);
+
+        i++;
+        if (i >= sequence.length) {
+            clearInterval(interval);
+        }
+   }, 600);
+}
+
+function lightUp(panel) { //panel is a string 'green'
+    var thisPanel = document.getElementById(panel);
+    thisPanel.classList.add('lit');
+    thisPanel.classList.add('animated', 'infinite','jello');
+    window.setTimeout(function() {
+        thisPanel.classList.remove('lit');
+        thisPanel.classList.remove('animated', 'infinite','jello');
+    }, animationDuration);
 
 }
-let memory = [];
 
-function playRound(){
-  let randomNum =Math.floor((Math.random() * 4) + 1)
-  memory.push(randomNum);
+function playSound(panel) {
+  const audio = document.querySelector(`audio[data-key="${panel}"]`);
+  audio.currentTime = 0;
+  audio.playbackRate = playSpeed;
+  audio.play();
+}
+
+function checkPattern(){
 
 }
 
